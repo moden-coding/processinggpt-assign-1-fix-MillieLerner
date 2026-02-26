27e9a2ecfe8aa79250121dc566d7b55ec6f4248b
MillieLerner <28mlerner@gannacademy.org>
2026-02-26 12:23:28 -0500

    done

--- src/App.java
+++ src/App.java
@@ -4,4 +4,22 @@ public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
-}
+    public void setting(){
+        size(400,400);
+    }
+    public void setup(){
+        background(20,150,80);
+        fill(90,82,100);
+        stroke(255);
+        strokeWeight(2);
+        ellipse(50,50,50,50);
+        fill(255,255,255);
+        stroke(0);
+        strokeWeight(9);
+        rect(1,1,15,15);
+
+        strokeWeight(4);
+        line(16,16,200,200);
+    }
+    }
+
